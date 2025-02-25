0338ff8de3736ca20a0bac0924c96fb97df842fa
Aleksandar Chobanov <aleksandar.chobanov@sciant.com>
2025-02-25 22:08:39 +0200

    Basics with TypeScript Intro

---
+++ index.ts
@@ -0,0 +1,49 @@
+import { result, sum } from "cypress/types/lodash";
+
+// let vs const
+let number: number = 5; 
+number = 10;
+
+const message: string = "Welcome"
+
+
+//String
+let welcomeMessage: string = "Hello Peter!"
+let test: string = "Ivan"
+
+let welcomeMessageLenght: number = welcomeMessage.length;
+
+
+// string interpolation
+let welcomeMessageDetails: string = 'My welcome message lenght is ${welcomeMessageLenght}';
+let userWelcomeMessage: string = 'Welcome, ${test}';
+
+// number
+let firstNumber: number = 5;
+let secondNumber: number = 10;
+
+//Number: math operations
+let add: number = secondNumber + firstNumber; 
+let substract: number = secondNumber - firstNumber;
+let multiply: number = secondNumber * firstNumber;
+let divide: number = secondNumber / firstNumber;
+
+// Boolean (true/false)
+
+let isTrue: boolean = true;
+let isGreater: boolean = firstNumber > secondNumber; 
+let isLower: boolean = firstNumber < secondNumber;
+let isEqual: boolean = firstNumber === secondNumber;
+let isLowerorEqual: boolean = firstNumber <= secondNumber;
+
+let areNumbersEqualTo: boolean = 11 === firstNumber && 10 === secondNumber;
+let isAnyNumberEqualTo: boolean = 6 === firstNumber || 1 === secondNumber;
+
+
+// Any
+let stringValue: any = "Hello"
+stringValue = 10;
+stringValue = 10 === 10;
+stringValue = [10, 20, 30];
+
+console.log("stringValue", stringValue);
